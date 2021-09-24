82c6a4d007a6db87a0732669cf9bc66c5fa9f665
John Chishugi <jkchishugi@gmail.com>
2021-09-24 19:50:55 +0100

    ft-create-migration-books-authors

---
+++ database/migrations/2021_09_24_184319_create_authors_books_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAuthorsBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('authors_books', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('book_id'); 
+            $table->bigInteger('author_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('authors_books');
+    }
+}
