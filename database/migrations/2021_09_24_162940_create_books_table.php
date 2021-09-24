070390224968f816a6a17a8b3773a4f85e506107
John Chishugi <jkchishugi@gmail.com>
2021-09-24 18:01:50 +0100

    ft-books-migration

---
+++ database/migrations/2021_09_24_162940_create_books_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('isbn');
+            $table->string('country');
+            $table->integer('author_id');
+            $table->integer('number_of_pages');
+            $table->string('publisher');
+            $table->string('release_date');
+            $table->timestamps();
+            // $table->foreign('author_id')->references('id')->on('authors')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
