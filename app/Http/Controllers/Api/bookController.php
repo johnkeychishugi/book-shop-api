dce5e24b2d5bd90497368519f07e21e6182babd5
John Chishugi <jkchishugi@gmail.com>
2021-09-24 21:28:34 +0100

    ft-get-books

--- app/Http/Controllers/Api/bookController.php
+++ app/Http/Controllers/Api/bookController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Api;
 
 use Response;
 use App\Http\Controllers\Controller;
+use App\Models\Author;
+use App\Models\Book;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
@@ -24,7 +26,7 @@ class BookController extends Controller
            });
         }
 
-        $data = $this->formattageResponse($books);
+        $data = $this->formattageResponse($books,2);
 
         return Response::json([
             'success_code' => 200,
@@ -33,16 +35,78 @@ class BookController extends Controller
         ], 200);
     }
 
-    public function formattageResponse($books)
+    public function createBook(Request $request)
+    {
+        $this->validate($request,[
+            'name' => 'required',
+            'isbn' => 'required',
+            'country' => 'required',
+            'authors' => 'required',
+            'number_of_pages' => 'required|numeric',
+            'publisher' => 'required',
+            'release_date' => 'required',
+        ]);
+
+        $author = Author::whereName($request->authors)->first();
+
+        if(!$author){
+            $author = new Author();
+            $author->name = $request->authors;
+            $author->save();
+        }
+
+        $book = new Book();
+        $book->name = $request->name;
+        $book->isbn = $request->isbn;
+        $book->country = $request->country;
+        $book->number_of_pages = $request->number_of_pages;
+        $book->publisher = $request->publisher;
+        $book->release_date = $request->release_date;
+        $book->save();
+        $book->authors()->attach($author->id);
+
+        $data= Book::whereId($book->id)->with('authors')->get();
+
+        $data = $this->formattageResponse($data,1);
+
+        return Response::json([
+            'success_code' => 201,
+            'status' => 'success',
+            'data' => $data
+        ],201);
+    }
+
+    public function getBooks()
+    {
+        $books = Book::with('authors')->get();
+
+        $data = $this->formattageResponse($books,1);
+
+        return Response::json([
+            'success_code' => 200,
+            'status' => 'success',
+            'data' => $data
+        ],200);
+    }
+
+    public function formattageResponse($books, $from)
     {
          $data = [];
 
          // Let's Map the results  for formattage of data
-         $formatData = $books->map(function ($item) {
+         $formatData = $books->map(function ($item) use ($from) {
                 $data['name'] = $item->name;
                 $data['isbn'] = $item->isbn;
-                $data['authors'] = $item->authors;
-                $data['number_of_pages'] = $item->numberOfPages;
+                //if $from is 1 data coming from internal and other if from external
+                if($from == 1){
+                    $data['authors'] = $item->authors->map(function($value){
+                          return $value->name;
+                    });
+                    $data['number_of_pages'] = $item->number_of_pages;
+                }else{
+                    $data['authors'] = $item->authors;
+                    $data['number_of_pages'] = $item->numberOfPages;
+                }
                 $data['publisher'] = $item->publisher;
                 $data['country'] = $item->country;
                 $data['release_date'] = date('Y-m-d', strtotime($item->released));
