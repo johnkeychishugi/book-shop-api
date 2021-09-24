13956a5df9773ccde2385d6ea5eba06a2a75e542
John Chishugi <jkchishugi@gmail.com>
2021-09-24 17:27:32 +0100

    ft-get-book-external-book-done

--- app/Http/Controllers/Api/bookController.php
+++ app/Http/Controllers/Api/bookController.php
@@ -17,36 +17,39 @@ class BookController extends Controller
         $books = Http::get('https://www.anapioficeandfire.com/api/books')->object();
         $books = collect($books);
 
-        // The array we're going to return
-        $data = [];
-
-        // Let's Map the results  for formattage of data
-        $formatData = $books->map(function ($item) {
-               $data['name'] = $item->name;
-               $data['isbn'] = $item->isbn;
-               $data['authors'] = $item->authors;
-               $data['number_of_pages'] = $item->numberOfPages;
-               $data['publisher'] = $item->publisher;
-               $data['country'] = $item->country;
-               $data['release_date'] = date('Y-m-d', strtotime($item->released));
-
-               return $data;
-            });
-
-        
         //If the parameter name exist
         if ($name) {
-            $data = collect($formatData);
-
-            $formatData = $data->filter(function ($item){
-                  return $item->name == $name;
-            });
+           $books = $books->filter(function ($item,$key) use ($name) {
+                return $item->name == $name;
+           });
         }
+
+        $data = $this->formattageResponse($books);
 
         return Response::json([
             'success_code' => 200,
             'status' => 'success',
-            'data' => $formatData
+            'data' => $data
         ], 200);
+    }
+
+    public function formattageResponse($books)
+    {
+         $data = [];
+
+         // Let's Map the results  for formattage of data
+         $formatData = $books->map(function ($item) {
+                $data['name'] = $item->name;
+                $data['isbn'] = $item->isbn;
+                $data['authors'] = $item->authors;
+                $data['number_of_pages'] = $item->numberOfPages;
+                $data['publisher'] = $item->publisher;
+                $data['country'] = $item->country;
+                $data['release_date'] = date('Y-m-d', strtotime($item->released));
+ 
+                return $data;
+             });
+        
+          return $formatData;
     }
 }
